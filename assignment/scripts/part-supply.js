e22b4fb2c0e8f99ca37c00aa4bfa380dea755fe2
Austin Kammerer <austin4191@gmail.com>
2021-08-31 18:37:44 -0500

    completed Q6 and added question

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -28,7 +28,16 @@ console.log(supplyChanges);
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
-
+console.log("6. Showing supplyChanges...");
+for (let i = 0; i < supplyChanges.length; i++) {
+  if (supplyChanges[i] > 0) {
+    console.log(`Added ${supplyChanges[i]} parts`);
+  } else if (supplyChanges[i] < 0) {
+    console.log(`Removed ${supplyChanges[i]} parts`);
+  } else {
+    console.log("No Change");
+  }
+}
 // STRETCH GOALS
 console.log("---  Stretch Goals  ---");
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
